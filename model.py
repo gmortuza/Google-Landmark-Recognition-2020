8ff8c316e111a5dd24b9be6b0bbb6dfda4229c53
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-09-16 14:56:32 -0600

    Base model done

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.applications import VGG19
+from tensorflow.keras.models import Model
+import pandas as pd
 
 #%%
 # Define constant
@@ -19,21 +21,18 @@ TRAIN_FILE = os.path.join(DATA_DIR, 'train.csv')
 #%%
 # Building model
 model = VGG19(
-    include_top=True,
+    include_top=False,
     weights="imagenet",
-    input_tensor=None,
-    input_shape=None,
-    pooling=None,
     classes=1000,
-    classifier_activation="softmax",
 )
+output = model.layers[-1].output
+output = tf.keras.layers.Flatten()(output)
+vgg_model = Model(model.input, output)
+vgg_model.trainable = False
+for layer in vgg_model.layers:
+    layer.trainable = False
 
-
-print(model)
-
-
-
-
-
-
+layers = [(layer, layer.name, layer.trainable) for layer in vgg_model.layers]
+layer_df = pd.DataFrame(layers, columns=["Layer type", "layer name", "layer trainable"])
+layer_df.head(10)
 
